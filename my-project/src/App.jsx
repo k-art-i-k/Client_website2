c378c61e73b6e3b75831fe4087a73188d0a967cd
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-10-18 09:44:19 +0530

    Add files via upload
    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function App() {
+
+
+  return (
+  <>
+     <Navbar />
+      <div className="pt-16"> {/* Add padding to prevent overlap */}
+        {/* Your main content goes here */}
+      </div>
+  </>
+  )
+}
+
+export default App
---
+++ my-project/src/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-black text-white z-50">
+      <div className="container mx-auto flex justify-center items-center h-20">
+        <nav className="flex space-x-6">
+          <a href="#" className="hover:text-gray-400 px-4 border-r  border-gray-400 h-full flex items-center">
+            HOME
+          </a>
+          <a href="#" className="hover:text-gray-400 px-4 border-r border-gray-400 h-full flex items-center">
+            ABOUT
+          </a>
+          <a href="#" className="hover:text-gray-400 px-4 border-r border-gray-400 h-full flex items-center">
+            OUR SERVICES
+          </a>
+          <a href="#" className="hover:text-gray-400 px-4 border-r border-gray-400 h-full flex items-center">
+            CONTACT US
+          </a>
+          <a href="#" className="hover:text-gray-400 px-4 h-full flex items-center">
+            TRACKING
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
